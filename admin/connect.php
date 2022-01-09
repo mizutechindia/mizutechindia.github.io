c1548cd5847c3176ccddbda7aa4b951c18be6e87
grgauravweb <raig72212@gmail.com>
2022-01-09 18:35:34 +0530

    first commit

--- admin/connect.php
+++ admin/connect.php
@@ -1,11 +1,11 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
-$database="database_name";
-$con=mysqli_connect($server,$username,$password,$database);
-if(!$con){
-    die("Connection error". mysqli_connect_error());
-}
-header("admin.php");
+// $server="localhost";
+// $username="root";
+// $password="";
+// $database="database_name";
+// $con=mysqli_connect($server,$username,$password,$database);
+// if(!$con){
+//     die("Connection error". mysqli_connect_error());
+// }
+// header("admin.php");
 ?>
